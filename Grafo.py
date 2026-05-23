eaed4434553ec884c76935a7f18a0f2a449dff9b
Suelen Sá <suelensa@aluno.uespi.br>
2026-05-23 10:58:44 -0300

    Refactor GrafoMatrizAdjacencia initialization and methods

--- Grafo.py
+++ Grafo.py
@@ -5,7 +5,11 @@ class GrafoMatrizAdjacencia:
         self.num_vertices = num_vertices
         self.ponderado = ponderado
         self.num_arestas = 0
-        self.matriz = [[math.inf] * num_vertices for _ in range(num_vertices)]
+        
+        # Se for ponderado, posições vazias são Infinito. Se não, são 0.
+        valor_inicial = math.inf if ponderado else 0
+        self.matriz = [[valor_inicial] * num_vertices for _ in range(num_vertices)]
+        
         for i in range(num_vertices):
             self.matriz[i][i] = 0
 
@@ -18,7 +22,24 @@ class GrafoMatrizAdjacencia:
 
     def obter_vizinhos(self, u):
         u_idx = u - 1
-        return [i + 1 for i, peso in enumerate(self.matriz[u_idx]) if i != u_idx and peso != 0 and peso != math.inf]
+        vizinhos = []
+        for i, peso in enumerate(self.matriz[u_idx]):
+            if i != u_idx:  # Não é a diagonal principal
+                if self.ponderado:
+                    # No ponderado, vizinho válido não é INF e não é 0
+                    if peso != math.inf and peso != 0:
+                        vizinhos.append(i + 1)
+                else:
+                    # No NÃO ponderado, vizinho válido é quem tem conexão ativa (diferente de 0)
+                    if peso != 0:
+                        vizinhos.append(i + 1)
+        return vizinhos
+
+    def obter_peso(self, u, v):
+        return self.matriz[u - 1][v - 1]
+
+    def __repr__(self):
+        return f"GrafoMatrizAdjacencia(num_vertices={self.num_vertices}, ponderado={self.ponderado})"
 
     def obter_peso(self, u, v):
         return self.matriz[u - 1][v - 1]
